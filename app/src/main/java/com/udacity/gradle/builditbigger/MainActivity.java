9fec526b127e98bf183be0b31c5db23553ab1d22
stefan <stefansprenger@gmx.at>
2016-04-14 14:55:12 +0200

    set up real gce server

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MainActivityFragment fragment = new MainActivityFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_container, fragment)
+                    .commit();
     }
 
 
@@ -40,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
+
+        new JokeSupplyGCEAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
         Intent jokeShowIntent = new Intent(this, JokeShowActivity.class);
         JokeSupplier demoJokeSupplier = new JokeSupplier();
         jokeShowIntent.putExtra("JOKE_ID", demoJokeSupplier.getJoke());
---
+++ jokeserver/src/main/java/stefan/sprenger/jokesupplier/MyBean.java
@@ -0,0 +1,15 @@
+package stefan.sprenger.jokesupplier;
+
+/** The object model for the data we are sending through endpoints */
+public class MyBean {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+}
